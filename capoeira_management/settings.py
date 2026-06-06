4b9bb2047ae265ff431dc81c0a75556b45582d17
Thiago <thiagotpa7@gmail.com>
2026-06-06 17:10:06 -0300

    update footer

--- capoeira_management/settings.py
+++ capoeira_management/settings.py
@@ -18,6 +18,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
